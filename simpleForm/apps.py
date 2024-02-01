978c5a748f9b96955718275b50b1e2b1a9003a46
parmanand007 <parmanand.p@brimo.in>
2024-02-02 01:22:41 +0530

    use signas to send messsage instead of celery

--- simpleForm/apps.py
+++ simpleForm/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class SimpleformConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'simpleForm'
+    def ready(self):
+        import simpleForm.signals
---
+++ simpleForm/signals.py
@@ -0,0 +1,23 @@
+# signals.py
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.mail import send_mail
+from .models import User
+from django.conf import settings
+
+@receiver(post_save, sender=User)
+def send_form_submission_email(sender, instance, created, **kwargs):
+    """
+    Signal receiver function to send an email when a user object is saved.
+    """
+    if created:  # Only send email for newly created user objects
+        try:
+            send_mail(
+                'Form Submission Confirmation',
+                'Thank you for submitting the form!',
+                settings.SENDER_EMAIL, [instance.email],
+                fail_silently=False,
+            )
+        except Exception as e:
+            pass  # Handle email sending failure if needed
